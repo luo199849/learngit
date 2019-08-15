c9f9b39b5f36ce362c55517ce41424d71ba18f8e
luojian <1781596097@qq.com>
2019-08-15 22:00:10 +0800

    初始化代码

---
+++ src/main/java/com/imooc/mapper/UsersMapper.java
@@ -0,0 +1,9 @@
+package com.imooc.mapper;
+
+import com.imooc.pojo.Users;
+import com.imooc.utils.MyMapper;
+import org.apache.ibatis.annotations.Param;
+
+public interface UsersMapper extends MyMapper<Users> {
+//    int updateStatus(@Param("id") int id);
+}
